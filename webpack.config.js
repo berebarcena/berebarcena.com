e3f5966fb9a54b7911bfba279d6f491cff21dcbd
Marcos Mellado <marcos@mellados.com>
2017-07-18 19:02:10 +0200

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,22 @@ var babelPlugins = [];
 var webpackPlugins = [
   new CopyWebpackPlugin([
     { from: './src/index.html', to: './index.html' },
+    { from: './src/404.html', to: './404.html' },
     { from: './src/static', to: './static' }
   ]),
   new webpack.optimize.OccurrenceOrderPlugin(true)
 ];
 
 if (isProduction) {
+  // Production webpack plugins
   webpackPlugins.push(new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } }));
+  webpackPlugins.push(new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production')
+    }
+  }));
+
+  // Production babel plugins
   babelPlugins.push(["transform-react-remove-prop-types", {
     "mode": "wrap",
     "ignoreFilenames": ["node_modules"]
@@ -32,6 +41,7 @@ var config = {
     rules: [
       { // All JS/React files
         test: /\.js$/,
+        exclude: [/node-modules/],
         use: [{
           loader: 'babel-loader',
           options: {
